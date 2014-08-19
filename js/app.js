f1ec5ec64a84c5d3b44f3eafae13144656afbec0
Doug Grainger <dhgrainger@gmail.com>
2014-08-19 15:55:24 -0400

    layout done

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@
   });
 
   var gems = [
-
     {
       name: "Ruby",
       price: "3.95",
       description: "A really shiny gem",
-      canPurchase: true
+      canPurchase: true,
+      image: 'https://cdn.tutsplus.com/net/uploads/legacy/966_essentialReading/images/rubylang.png'
     },
 
     {
@@ -19,6 +19,7 @@
       price: 2.95,
       description: 'A very spiky gem',
       canPurchase: false,
+      image: 'http://upload.wikimedia.org/wikipedia/commons/thumb/6/66/POV-Ray-Dodecahedron.svg/280px-POV-Ray-Dodecahedron.svg.png'
     }
   ];
 })();
